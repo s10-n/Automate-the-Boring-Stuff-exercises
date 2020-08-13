d887e85a13a9a4834d0487b95d274ab7138feda6
s10 <68397573+s10-n@users.noreply.github.com>
2020-08-12 19:52:37 -0700

    Started work on strongPassword.py

---
+++ Chapter 7/strongPassword.py
@@ -0,0 +1,29 @@
+# strongPassword.py - detects whether a given password is strong
+
+# a strong password must be at least 8 characters long, contain both uppercase and lowercase characters, and have at least one digit
+
+import re
+
+lengthRegex = re.compile(r'(.{8,})+')
+uppercaseRegex = re.compile(r'.*[a-z].*')
+lowercaseRegex = re.compile(r'.*[A-Z].*')
+digitRegex = re.compile(r'.*\d.*')
+
+# passwordValidator(password)
+# takes in password, checks if it is strong
+# outputs True if strong and False if not
+
+def passwordValidator(password):
+    if lengthRegex.findall(password) != []: # test length
+        
+    # test uppercase
+    # test lowercase
+    # test digit
+    # if all true, return true
+
+passwordValidator("12345678") # False
+passwordValidator("One2345678") # True
+passwordValidator("one2345678") # False
+passwordValidator("Poop") # False
+passwordValidator("P00pcorn") # True
+passwordValidator("p00pcorn") # False
