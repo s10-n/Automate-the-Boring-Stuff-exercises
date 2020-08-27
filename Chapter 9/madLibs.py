88a488a2e289ec95802b466faba7c57eceba976c
Sean Beaudoin <sean@enkidu>
2020-08-26 23:47:08 -0700

    updated madlibs.py

--- Chapter 9/madLibs.py
+++ Chapter 9/madLibs.py
@@ -1,5 +1,7 @@
 # madLibs.py - reads a text file and lets the user add in their own text anywhere the words ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file
 import os
 from pathlib import Path
-os.chdir(r'.\Chapter 9')
-Path.cwd()
+# open a text file
+# find all instances of the words to be replaced
+# for each word to be replaced, prompt the user to replace them
+# when every placeholder word has been replaced, print the story and write it to a new text file
