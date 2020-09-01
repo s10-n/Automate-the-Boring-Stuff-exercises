b0f8d2448c0e73ae4a27c3663e1e1935977d6339
s10 <68397573+s10-n@users.noreply.github.com>
2020-08-31 17:57:27 -0700

    finished backupToZip.py

--- Chapter 10/backupToZip.py
+++ Chapter 10/backupToZip.py
@@ -7,11 +7,24 @@ def backupToZip(folder):
     # figure out the filename this code should use based on what files already exist
     number = 1
     while True:
-        zipFilename = os.path.basename(folder) + '_' + '.zip'
+        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
         if not os.path.exists(zipFilename):
             break
         number = number + 1
-    # todo: create the zip file
-    # todo walk the entire folder tree and compress the files in each folder
+    # create the zip file
+    print(f'Creating {zipFilename}...')
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
+    # walk the entire folder tree and compress the files in each folder
+    for foldername, subfolders, filenames in os.walk(folder):
+        print(f'Adding files in {foldername}...')
+        # add the current folder to the zip file
+        backupZip.write(foldername)
+        # add all the files in this folder to the zip file
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue # don't back up the backup zip files
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
     print('Done.')
-#backupToZip('c:/Users/User/mu_code/Automate the Boring Stuff/Chapter 10')
+backupToZip('c:/Users/User/mu_code/Automate the Boring Stuff/Chapter 10')
