d9a5338e5fc772d2f6201ca262c02408ee3e5e7b
s10 <68397573+s10-n@users.noreply.github.com>
2020-09-02 22:45:30 -0700

    completed fillingInTheGaps.py

--- Chapter 10/fillingInTheGaps.py
+++ Chapter 10/fillingInTheGaps.py
@@ -7,14 +7,17 @@ def gap_filler(prefix,target_folder): # string, path
     filename_regex = re.compile(r'\d+\b')
     # filters list by two lambdas, one which determines it has the correct prefix in it and one which determines it has a number in the last position of the filename
     filtered_list = list(filter(lambda file: re.search(filename_regex,file),list(filter(lambda file: prefix in file,os.listdir(target_folder)))))
-    for file_number,file in enumerate(filtered_list,start=1):
-        if file_number == int(re.search(filename_regex,Path(file).stem).group()):
-            print(int(re.search(filename_regex,Path(file).stem).group()))
-        #print(file_number)
- #       print(int(re.search(filename_regex,Path(file).stem).group()))
-    
-# list all of the files in a folder with a given prefix
-# iterate through those files in order to ensure that there is a number
-# if there is a gap, rename the files to close this gap
+    # iterates through the filtered list, which is enumerated to determine the correct number of the file
+    print(f"Numbered files with prefix '{prefix}':")
+    for idx,file in enumerate(filtered_list,start=1):
+        # collects the number from the filename
+        file_number = re.search(filename_regex,Path(file).stem).group()
+        # if the file's number doesn't match up with its place in the folder, it is renamed
+        if idx != int(file_number):
+            new_name = (prefix + ('0' * (len(file_number)-len(str(idx))) + str(idx) + str(Path(file).suffix)))
+            os.rename(target_folder + file, target_folder + new_name)
+            print(f'{file} renamed to {new_name}')
+        else:
+            print(file)
 
 gap_filler('spam','c:/Users/User/mu_code/Automate the Boring Stuff/Chapter 10/gap_filler files/')
