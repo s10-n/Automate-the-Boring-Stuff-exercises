beb217409b205e72a922b61c7bdc7d6d20d13b11
s10 <68397573+s10-n@users.noreply.github.com>
2020-09-02 23:25:08 -0700

    started working on creatingAGap.py

---
+++ Chapter 10/creatingAGap.py
@@ -0,0 +1,26 @@
+# creatingAGap.py - inserts a gap into numbered files after file n, with n as one of the variables passed to the function
+import os,re
+from pathlib import Path
+
+def gap_creator(prefix,target_folder,gap_target): # string, path, int
+    filename_regex = re.compile(r'\d+\b')
+    # filters list by two lambdas, one which determines it has the correct prefix in it and one which determines it has a number in the last position of the filename
+    filtered_list = list(filter(lambda file: re.search(filename_regex,file),list(filter(lambda file: prefix in file,os.listdir(target_folder)))))
+    # iterates through the filtered list, which is enumerated to determine the correct number of the file
+    print(f"Numbered files with prefix '{prefix}':")
+    name_changes = {}
+    for idx,file in enumerate(filtered_list,start=1):
+        # collects the number from the filename
+        file_number = re.search(filename_regex,Path(file).stem).group()
+        # if the file's number doesn't match up with its place in the folder, it is renamed
+        if idx > gap_target:
+            name_changes[file] = (prefix + ('0' * (len(file_number)-len(str(idx))) + str(idx+1) + str(Path(file).suffix)))
+            #os.rename(target_folder + file, target_folder + new_name)
+            #print(f'{file} renamed to {new_name}')
+        else:
+            print(file)
+    for filename in name_changes:
+        #os.rename(target_folder + filename, target_folder + name_changes[filename])
+        print(f'{filename} renamed to {name_changes[filename]}')
+
+gap_creator('spam','c:/Users/User/mu_code/Automate the Boring Stuff/Chapter 10/gap_filler files/',2)
