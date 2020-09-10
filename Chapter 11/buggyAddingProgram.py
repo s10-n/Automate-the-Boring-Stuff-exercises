e2559ea7b663803317ad0a8bca9aa561803ec379
s10 <68397573+s10-n@users.noreply.github.com>
2020-09-09 23:07:01 -0700

    Started chapter 11

---
+++ Chapter 11/buggyAddingProgram.py
@@ -0,0 +1,8 @@
+print('Enter the first number to add:')
+first = input()
+print('Enter the second number:')
+second = input()
+print('Enter the third number:')
+third = input()
+breakpoint()
+print('The sum is ' + first + second + third)
