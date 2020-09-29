a5a00e6c0dd6fc86f589f2e35d9ff400f3fe8566
s10 <68397573+s10-n@users.noreply.github.com>
2020-09-28 23:56:35 -0700

    added check for image results

--- Chapter 12/imageSiteDownloader.py
+++ Chapter 12/imageSiteDownloader.py
@@ -6,10 +6,7 @@ import os,requests
 from pathlib import Path
 
 def image_site_downloader(search_term):
-
-    # create a directory for the search term in the current working directory
-    os.makedirs(search_term,exist_ok=True)
-    
+  
     # open the Imgur search page for the search term in Firefox
     browser = webdriver.Firefox()
     browser.get('https://imgur.com/search?q=' + search_term)
@@ -18,23 +15,32 @@ def image_site_downloader(search_term):
     # note that image URLs on the search page have a 'b' in them that denotes their status as thumbnails
     result_posts = browser.find_elements_by_css_selector('[src$="b.jpg"]')
 
-    # remove the 'b' from each thumbnail URL to get the full size image's URL
-    for thumbnail in result_posts:
-        split_image_url = thumbnail.get_attribute('src').split('b.')
-        image_url = '.'.join(split_image_url)
+    # count the number of posts found and exit program if 0
+    if not len(result_posts):
+        print('0 results found for "' + search_term + '" on Imgur.')
+    else:
+        print(str(len(result_posts)) + ' results found for "' + search_term + '" on Imgur.')
 
-        # download the image and save it locally
-        online_image_file = requests.get(image_url)
-        online_image_file.raise_for_status()
-        local_image_path = os.path.join(search_term,os.path.basename(image_url))
-        local_image_file = open(local_image_path,'wb')
-        for chunk in online_image_file.iter_content(100000):
-            local_image_file.write(chunk)
+        # create a directory for the search term in the current working directory
+        os.makedirs(search_term,exist_ok=True)
 
-        # notify the user that the file has been saved
-        print(os.path.basename(image_url) + ' saved to ' + str(os.path.join(Path.cwd(),search_term)))
+        # remove the 'b' from each thumbnail URL to get the full size image's URL
+        for thumbnail in result_posts:
+            split_image_url = thumbnail.get_attribute('src').split('b.')
+            image_url = '.'.join(split_image_url)
 
-        # close the local image file
-        local_image_file.close() 
+            # download the image and save it locally
+            online_image_file = requests.get(image_url)
+            online_image_file.raise_for_status()
+            local_image_path = os.path.join(search_term,os.path.basename(image_url))
+            local_image_file = open(local_image_path,'wb')
+            for chunk in online_image_file.iter_content(100000):
+                local_image_file.write(chunk)
+
+            # notify the user that the file has been saved
+            print(os.path.basename(image_url) + ' saved to ' + str(os.path.join(Path.cwd(),search_term)))
+
+            # close the local image file
+            local_image_file.close() 
 
 image_site_downloader(input('Type a search term: '))
