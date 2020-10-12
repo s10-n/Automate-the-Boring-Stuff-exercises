1ae04649b7e173f8c88a323181bf27dc7e8febe5
Sean Beaudoin <s10_n@mailbox.org>
2020-10-12 09:43:11 -0700

    finished blankRowInserter.py

--- Chapter 13/blankRowInserter.py
+++ Chapter 13/blankRowInserter.py
@@ -2,21 +2,37 @@
 # blankRowInserter.py - takes two integers (N and M) and a spreadsheet filename string as command line arguments, and starting at row N, the program inserts M blank rows into the spreadsheet
 
 import openpyxl,os,sys
+from openpyxl.utils import get_column_letter
 from pathlib import Path
 
-#N = int(sys.argv[1])
-#M = int(sys.argv[2])
-#filename_string = sys.argv[3]
-N = 3
-M = 2
-filename_string = '/home/sean/projects/automate-the-boring-stuff-exercises/Chapter 13/9x9_Multiplication_Table.xlsx'
+N = int(sys.argv[1])
+M = int(sys.argv[2])
+filename_string = sys.argv[3]
 
 # read the contents of the spreadsheet
 spreadsheet = openpyxl.load_workbook(filename_string)
+print('Opened ' + Path(filename_string).name + '.')
 sheet = spreadsheet.active
-max_row = sheet.max_row
-max_column = sheet.max_column
 
-# use a for loop to copy the first N lines
-# add M to the row number for the remaining lines in the output spreadsheet
-#spreadsheet.save(Path(filename_string).stem + '_copy.xlsx')
+# assign the rows to be divided to variables
+first_N_lines = list(sheet.rows)[:N-1]
+remaining_lines = list(sheet.rows)[N:]
+
+# create a new spreadsheet 
+new_spreadsheet = openpyxl.Workbook()
+new_sheet = new_spreadsheet.active
+
+# write the first N-1 lines to the new spreadsheet unchanged
+for row in first_N_lines:
+    for cell in row:
+        new_sheet[cell.coordinate] = cell.value
+
+# move the remaining rows down by M to create the break
+for row in remaining_lines:
+    for cell in row:
+        new_sheet[get_column_letter(cell.column) + str(cell.row + M)] = cell.value
+
+# save the new spreadsheet
+new_filename = Path(filename_string).stem + '_copy.xlsx'
+new_spreadsheet.save(new_filename)
+print(f'Saved as {new_filename}.')
